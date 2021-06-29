82e37dd549d309735f98e041fc6f9e637f8e8bd5
Lapitskyi <lapitskiy4@gmail.com>
2021-06-29 15:00:43 +0300

    fix crypto app

--- src/App.vue
+++ src/App.vue
@@ -1,288 +1,115 @@
 <template>
-  <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-    <div class="container">
-      <section>
-        <div class="flex">
-          <div class="max-w-xs">
-            <label for="wallet" class="block text-sm font-medium text-gray-700"
-              >Тикер</label
-            >
-            <div class="mt-1 relative rounded-md shadow-md">
-              <input
-                v-model="ticker"
-                @keydown.enter="addCrypto"
-                type="text"
-                name="wallet"
-                id="wallet"
-                class="
-                  block
-                  w-full
-                  pr-10
-                  border-gray-300
-                  text-gray-900
-                  focus:outline-none focus:ring-gray-500 focus:border-gray-500
-                  sm:text-sm
-                  rounded-md
-                "
-                placeholder="Например DOGE"
-              />
-            </div>
-            <div
-              v-if="ticker !== ''"
-              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
-            >
-              <span
-                v-for="option in optionsTicker[0]"
-                :key="option.id"
-                @click="addCrypto"
-                class="
-                  inline-flex
-                  items-center
-                  px-2
-                  m-1
-                  rounded-md
-                  text-xs
-                  font-medium
-                  bg-gray-300
-                  text-gray-800
-                  cursor-pointer
-                "
-              >
-                {{ option.symbol }}
-              </span>
-            </div>
-            <div v-if="flag" class="text-sm text-red-600">
-              Такой тикер уже добавлен
-            </div>
-          </div>
-        </div>
-        <button
-          @click="addCrypto"
-          type="button"
-          class="
-            my-4
-            inline-flex
-            items-center
-            py-2
-            px-4
-            border border-transparent
-            shadow-sm
-            text-sm
-            leading-4
-            font-medium
-            rounded-full
-            text-white
-            bg-gray-600
-            hover:bg-gray-700
-            transition-colors
-            duration-300
-            focus:outline-none
-            focus:ring-2
-            focus:ring-offset-2
-            focus:ring-gray-500
-          "
-        >
-          <!-- Heroicon name: solid/mail -->
-          <svg
-            class="-ml-0.5 mr-2 h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="#ffffff"
-          >
-            <path
-              d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-            ></path>
-          </svg>
+  <div class="container">
+    <section>
+      <form v-on:submit.prevent class="form">
+        <label class="form__label" for="formInput">
+          <input
+            v-model="value"
+            @keydown.enter="addCrypto"
+            class="form__input"
+            type="text"
+            name="formInput"
+            id="formInput"
+            placeholder="Crypto name"
+          />
+        </label>
+        <button @click="addCrypto" type="button" class="form__btn">
+          <img src="" alt="" />
           Добавить
         </button>
-      </section>
-      <template v-if="tickers.length">
-        <hr class="w-full border-t border-gray-600 my-4" />
-        <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-          <div
-            v-for="t in tickers"
-            :key="t.id"
-            @click="selectedCrypto(t)"
-            :class="{
-              'border-4': sel === t,
-            }"
-            class="
-              bg-white
-              overflow-hidden
-              shadow
-              rounded-lg
-              border-purple-800 border-solid
-              cursor-pointer
-            "
-          >
-            <div class="px-4 py-5 sm:p-6 text-center">
-              <dt class="text-sm font-medium text-gray-500 truncate">
-                {{ t.name }} - USD
-              </dt>
-              <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                {{ t.price }}
-              </dd>
-            </div>
-            <div class="w-full border-t border-gray-200"></div>
-            <button
-              @click.stop="removeCrypto(t.id)"
-              class="
-                flex
-                items-center
-                justify-center
-                font-medium
-                w-full
-                bg-gray-100
-                px-4
-                py-4
-                sm:px-6
-                text-md text-gray-500
-                hover:text-gray-600 hover:bg-gray-200 hover:opacity-20
-                transition-all
-                focus:outline-none
-              "
-            >
-              <svg
-                class="h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="#718096"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              Удалить
-            </button>
-          </div>
-        </dl>
-        <hr class="w-full border-t border-gray-600 my-4" />
-      </template>
-      <section v-if="sel" class="relative">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ sel.name }} - USD
-        </h3>
-        <div class="flex items-end border-gray-600 border-b border-l h-64">
-          <div
-            v-for="(bar, idx) in normalizeGraph()"
-            :key="idx"
-            :style="{ height: `${bar}%` }"
-            class="bg-purple-800 border w-10"
-          >
-            {{ bar.price }}
-          </div>
-        </div>
-        <button
-          @click="sel = null"
-          type="button"
-          class="absolute top-0 right-0"
+      </form>
+    </section>
+
+    <section>
+      <ul class="crypto__list">
+        <li
+          v-for="crypto in cryptoArray"
+          :key="crypto.id"
+          @click="selectedCrypto(crypto)"
+          :class="{
+            active: sel === crypto,
+          }"
+          class="crypto__list-item"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            version="1.1"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background: new 0 0 512 512"
-            xml:space="preserve"
+          <div class="crypto__list-box">
+            <h2 class="crypto__list-title">{{ crypto.name }} - USD</h2>
+            <div class="crypto__list-price">{{ crypto.price }}</div>
+          </div>
+          <button
+            @click.stop="deleteCrypto(crypto.id)"
+            class="crypto__list-btn"
+            type="button"
           >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
-    </div>
+            <svg class="crypto__list-icon">
+              <use href="" />
+            </svg>
+            Delete
+          </button>
+        </li>
+      </ul>
+    </section>
+    <section v-if="sel">
+      <h2 class="graph__title">{{ sel.name }} - USD</h2>
+      <div class="graph">
+        <div
+          v-for="(bar, idx) in normalizeGraph()"
+          :key="idx"
+          :style="{ height: `${bar}%` }"
+          class="graph__item"
+        >
+          {{ bar.price }}
+        </div>
+      </div>
+    </section>
   </div>
 </template>
-
 <script>
+// import sprite from "./assets/svgSprite/svgSprite.svg"
 export default {
   name: "App",
   data() {
     return {
-      ticker: "",
-      tickers: [],
-      sel: null,
+      value: "",
+      cryptoArray: [],
       graph: [],
-      optionsTicker: [],
-      flag: false,
+      sel: null,
     };
   },
-  created() {
-    this.fetchData();
-  },
+  created() {},
   methods: {
-    requestTickers(currentTicker) {
+    requestCrypto(currentCrypto) {
       setInterval(async () => {
         const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_Key=521ed0233646da7d1d8ff9853bb38ec55bd426f19b2751886882897b744db609`
+          `https://min-api.cryptocompare.com/data/price?fsym=${currentCrypto.name}&tsyms=USD&api_Key=process.env.VUE_APP__API_KEY`
         );
         const data = await f.json();
-        this.tickers.find((itemTicker) => {
-          if (itemTicker.name === currentTicker.name) {
+        this.cryptoArray.find((itemTicker) => {
+          if (itemTicker.name === currentCrypto.name) {
             itemTicker.price =
               data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
           }
-          if (this.sel?.name === currentTicker.name) {
+          if (this.sel?.name === currentCrypto.name) {
             this.graph.push(data.USD);
           }
         });
       }, 5000);
     },
-    async fetchData() {
-      try {
-        const response = await fetch(
-          `https://min-api.cryptocompare.com/data/all/coinlist?summary=true`
-        );
-        const data = await response.json();
-        const cryptoTicket = Object.values(data.Data).map((item) => ({
-          id: item.Id,
-          name: item.FullName,
-          symbol: item.Symbol,
-        }));
-        this.optionsTicker.push(cryptoTicket);
-      } catch (e) {
-        console.log(e.response);
-      }
-    },
     addCrypto() {
-      if (this.ticker !== "") {
-        const currentTicker = {
+      if (this.value !== "") {
+        const currentCrypto = {
           id: Date.now(),
-          name: this.ticker,
+          name: this.value,
           price: "-",
         };
-        this.tickers?.find((tickersItem) => {
-          if (tickersItem !== currentTicker.name) {
-            // this.tickers.push(currentTicker);
-            console.log("Добавлен");
-          }
-          return (this.flag = true);
-        });
-
-        this.requestTickers(currentTicker);
-        this.tickers.push(currentTicker);
-
-        this.ticker = "";
+        this.requestCrypto(currentCrypto);
+        this.cryptoArray.push(currentCrypto);
+        this.value = "";
       }
     },
-    removeCrypto(idCrypto) {
-      this.tickers = this.tickers.filter((ticker) => ticker.id !== idCrypto);
-      this.sel = null;
+    deleteCrypto(cryptoId) {
+      this.cryptoArray = this.cryptoArray.filter(
+        (item) => item.id !== cryptoId
+      );
     },
     selectedCrypto(crypto) {
       this.sel = crypto;
@@ -298,5 +125,133 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+@import "./assets/scss/null.scss";
 
-<style src="./app.css"></style>
+.container {
+  max-width: 1400px;
+  margin: 0 auto;
+  padding: 10px;
+}
+
+section + section {
+  margin: 20px 0;
+  padding: 20px 0;
+}
+
+.form {
+  &__label {
+  }
+
+  &__input {
+    padding: 10px;
+    border: 1px solid #718096;
+    border-radius: 8px;
+  }
+
+  &__btn {
+    margin-left: 5px;
+    background-color: #718096;
+    color: #fff;
+    padding: 10px;
+    border-radius: 8px;
+  }
+}
+
+.active {
+  border-color: green !important;
+}
+
+.crypto {
+  &__list {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+    grid-template-rows: minmax(170px, auto);
+    grid-gap: 10px;
+
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  &__list-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    text-align: center;
+    border: 4px solid #a0aec0;
+    border-radius: 8px;
+  }
+
+  &__list-box {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+  }
+
+  &__list-title {
+    font-size: 14px;
+    color: #a0aec0;
+    text-align: center;
+    text-transform: uppercase;
+  }
+
+  &__list-price {
+    font-size: 30px;
+    color: #000000;
+  }
+
+  &__list-btn {
+    display: inline-block;
+    text-decoration: none;
+    text-align: center;
+    margin: 0;
+    border: none;
+    outline: none;
+    text-transform: uppercase;
+    white-space: nowrap;
+    width: 100%;
+    background-color: #a0aec0;
+    font-size: 16px;
+    color: #ffffff;
+    padding: 20px;
+    position: relative;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  &__list-icon {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+  }
+}
+.graph {
+  margin-top: 20px;
+  display: flex;
+  align-items: flex-end;
+  height: 200px;
+  border-left: 1px solid #000000;
+  border-bottom: 1px solid #000000;
+  &__title {
+    font-size: 16px;
+    text-transform: uppercase;
+  }
+
+  &__item {
+    width: 10px;
+    border: 1px solid purple;
+    background-color: purple;
+  }
+  &__item + &__item {
+    margin-left: 5px;
+  }
+}
+</style>
